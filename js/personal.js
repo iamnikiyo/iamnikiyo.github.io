3eed3c80e5be2444d3eade16c3e5e1b12a6edd58
Nicolas Escobar <Nicolas Escobar>
2016-11-25 09:44:48 +0100

    added sidebar(now In development)

---
+++ js/personal.js
@@ -0,0 +1,31 @@
+		$(document).ready(function(){
+        $("#goDown").on('click', function(event) {
+          if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+              scrollTop: $(hash).offset().top
+            }, 800, function(){
+              window.location.hash = hash;
+            });
+          } 
+        });
+
+      var movementStrength = 25;
+      var height = movementStrength / $(window).height();
+      var width = movementStrength / $(window).width();
+      $("#header").mousemove(function(e){
+                var pageX = e.pageX - ($(window).width() / 2);
+                var pageY = e.pageY - ($(window).height() / 2);
+                var newvalueX = width * pageX * -1 - 25;
+                var newvalueY = height * pageY * -1 - 50;
+                $('#bimage').css("background-position", newvalueX+"px     "+newvalueY+"px");
+      });
+      
+
+      $('#header').css("height",$(window).height() + "px");
+      $('#main').css("top",$(window).height() + "px");
+      $('#main').css("min-height",$(window).height() + "px");
+
+
+});
