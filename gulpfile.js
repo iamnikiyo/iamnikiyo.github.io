489f82d8109b3c65b595c8bbd11ac96c475fbe12
iamnikiyo <nikiyo9@gmail.com>
2016-12-17 00:54:43 +0100

    Restructuring the project, change from .scss to .sass. No finish yet

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,21 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('./css/'))
 });
 
+gulp.task('stylesSass',function(){
+    gulp.src('./css/*.sass')
+      .pipe(sass().on('error',sass.logError))
+      .pipe(gulp.dest('./css/'))
+});
+
 gulp.task('default', () => {
     browserSync.init({
         server: './'
     });
     gulp.watch('./*.html').on('change',browserSync.reload);
     gulp.watch('./css/*.scss',['styles']);
+    gulp.watch('./css/*.sass',['stylesSass']);
+    gulp.watch('./css/modules/*.sass',['stylesSass']);
+    gulp.watch('./css/base/*.sass',['stylesSass']);
     gulp.watch('./css/*.css').on('change',browserSync.reload);
     gulp.watch('./js/*.js').on('change',browserSync.reload);
 });
-
